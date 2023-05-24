1bf2c8c86c075ecb8e01dae6c26ea665223c367a
juan ramirez arias <jcra.juancamilo@gmail.com>
2023-05-23 20:22:01 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructores
+{
+    internal class Punto
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        // El constructor tiene el nombre de la clase
+        public Punto()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Punto(int x, int y)
+        {
+            X = x;
+            Y = y;
+            
+        }
+
+        public Punto(int x)
+        {
+            X = x;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Constructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto punto1 = new Punto();
+            Console.WriteLine($"x = {punto1.X} y = {punto1.Y}");
+
+            Punto punto2 = new Punto(5, 6);
+            Console.WriteLine($"x = {punto2.X} y = {punto2.Y}");
+
+            Punto punto3 = new Punto(8,9);
+            Console.WriteLine($"x = {punto3.X} y = {punto3.Y}");
+        }
+    }
+}
